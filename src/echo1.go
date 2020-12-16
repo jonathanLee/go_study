532b8b30b8b0ecbec14d7c41378a066af85bfd5c
jspman <test1234>
2020-12-17 01:23:27 +0900

    변수선언 방법, for 문에 대한 방법

---
+++ src/echo1.go
@@ -0,0 +1,22 @@
+/*
+ * Go 언어는 세미콜론이 필요없다.
+//+= 연산자는 s += sep + os.Args[i]
+ * s = s + sep+ os.Args[i]
+ * 변수 선언 방법
+ * s := ""
+*/
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	//var s, sep string
+	s, sep := "", ""
+	//string 변수 s, sep 선언
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
